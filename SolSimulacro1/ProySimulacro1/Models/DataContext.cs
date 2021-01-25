69dd0dda848536ecaa604dbaae56f06e35998acd
María René Ruiz Flores <a2019117746@estudiantes.upsa.edu.bo>
2021-01-25 13:49:20 -0400

    Se hizo un controlador para cada modelo

--- SolSimulacro1/ProySimulacro1/Models/DataContext.cs
+++ SolSimulacro1/ProySimulacro1/Models/DataContext.cs
@@ -12,5 +12,15 @@ namespace ProySimulacro1.Models
         {
 
         }
+
+        public System.Data.Entity.DbSet<ProySimulacro1.Models.Country> Countries { get; set; }
+
+        public System.Data.Entity.DbSet<ProySimulacro1.Models.Currency> Currencies { get; set; }
+
+        public System.Data.Entity.DbSet<ProySimulacro1.Models.Language> Languages { get; set; }
+
+        public System.Data.Entity.DbSet<ProySimulacro1.Models.Translations> Translations { get; set; }
+
+        public System.Data.Entity.DbSet<ProySimulacro1.Models.RegionalBloc> RegionalBlocs { get; set; }
     }
 }
